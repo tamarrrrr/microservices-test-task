0331b792f9b000795fc0071a08e9aa8c267ecd94
Tamara Fidarova <tamaraf@vosesoftware.com>
2022-08-01 17:07:41 +0400

    initial: server added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using helloworldserver;
+
+var url = "http://localhost:8080/";
+// Create a Http server and start listening for incoming connections
+SimpleServer.serverListener = new System.Net.HttpListener();
+SimpleServer.serverListener.Prefixes.Add(url);
+SimpleServer.serverListener.Start();
+Console.WriteLine("Listening for connections on {0}", url);
+
+// Handle requests
+Task listenTask = SimpleServer.HandleIncomingConnections();
+listenTask.GetAwaiter().GetResult();
+
+// Close the listener
+SimpleServer.serverListener.Close();
---
+++ SimpleServer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace helloworldserver
+{
+    public class SimpleServer
+    {
+        public static HttpListener serverListener;
+        public static string url = "http://localhost:8080/";        
+
+        public static async Task HandleIncomingConnections()
+        {
+            bool runServer = true;
+
+            while (runServer)
+            {
+                // wait for request
+                HttpListenerContext context = await serverListener.GetContextAsync();
+
+                // Peel out the requests and response objects
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+
+                // If `shutdown` url requested w/ POST, then shutdown the server after serving the page
+                if (request?.Url?.AbsolutePath == "/shutdown")
+                {
+                    Console.WriteLine("Shutdown requested");
+                    runServer = false;
+                }
+
+                // Write the response info                    
+                string? greetingName = String.IsNullOrEmpty(Environment.GetEnvironmentVariable("GREETING_NAME")) ? Environment.GetEnvironmentVariable("GREETING_NAME") : "Anonymous";
+                byte[] output = Encoding.UTF8.GetBytes($"Hello, {greetingName}");
+                response.ContentType = "text/html";
+                response.ContentEncoding = Encoding.UTF8;
+                response.ContentLength64 = output.LongLength;
+                await response.OutputStream.WriteAsync(output, 0, output.Length);
+                response.Close();
+            }
+        }
+    }
+}
